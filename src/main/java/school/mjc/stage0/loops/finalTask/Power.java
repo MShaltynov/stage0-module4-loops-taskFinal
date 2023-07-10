6027e5fd43a95feb0627d2ff3585840f5e7a2f27
Mikhail <shaltynovm@gmail.com>
2023-07-11 00:24:45 +0530

    initial commit

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+int mul=0;
+        mul=numberToPrint;
+        for (int i = 1; i < power; i++) {
+            numberToPrint=numberToPrint*mul;
+        }
 
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,29 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int width = cathetusLength * 2 - 1;
+
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < width; j++) {
+                if (j < cathetusLength) {
+                    if (j >= cathetusLength - i - 1) {
+                        System.out.print(cathetusLength - j);
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else if (j >= cathetusLength && (j < width + 1 - cathetusLength + i)) {
+                    System.out.print(j - cathetusLength + 2);
+                } else {
+
+                }
+            }
+
+            System.out.println();
+        }
 
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(5);
     }
 }
